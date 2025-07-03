7e0173cd757b454d507b9fa9d04b01a9370b34e2
thevpc <taha.bensalah@gmail.com>
2025-07-03 19:14:41 +0100

    [FIXED] changed to use @NApp.Info

--- modules/kifkif-swing/src/main/java/net/thevpc/kifkif/Kkc.java
+++ modules/kifkif-swing/src/main/java/net/thevpc/kifkif/Kkc.java
@@ -24,7 +24,8 @@ import net.thevpc.nuts.util.NMsg;
  * Date: 5 janv. 2005
  * Time: 21:02:48
  */
-public final class Kkc implements NApplication {
+@NApp.Info
+public final class Kkc  {
     public Kkc() {
 
     }
@@ -33,7 +34,7 @@ public final class Kkc implements NApplication {
         NApp.builder(args).run();
     }
 
-    @Override
+    @NApp.Main
     public void run() {
         NApp.of().runCmdLine(new NCmdLineRunner() {
             Options options = new Options();
